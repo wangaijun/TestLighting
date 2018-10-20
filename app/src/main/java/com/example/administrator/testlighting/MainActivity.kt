e9c048061e313ad01c4f49f82d34d58b5a33fda5
hw_wangaijun@163.com <waj67011>
2018-10-21 07:55:58 +0800

    添加事件处理程序

--- app/src/main/java/com/example/administrator/testlighting/MainActivity.kt
+++ app/src/main/java/com/example/administrator/testlighting/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.administrator.testlighting
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setViewsListener()
+    }
+
+    private fun setViewsListener() {
+        btnPrintAll.setOnClickListener{}
+        btnPrintQuestion.setOnClickListener{}
+        btnUseWordsAnswerQuestion.setOnClickListener{}
+        btnUseNumberAnswerQuestion.setOnClickListener{}
+        btnTest.setOnClickListener{}
     }
 }
