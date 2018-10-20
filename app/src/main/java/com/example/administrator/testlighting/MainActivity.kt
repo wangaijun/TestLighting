005bea656a2a0cd65c6f10afebd2988793fbd6c2
hw_wangaijun@163.com <waj67011>
2018-10-21 07:25:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/testlighting/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.administrator.testlighting
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
