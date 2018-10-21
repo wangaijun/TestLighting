acc41cb48aa84aff42b6dd4b253a335d7d777b5b
hw_wangaijun@163.com <waj67011>
2018-10-21 08:21:15 +0800

    显示全部问题

---
+++ app/src/main/java/com/example/administrator/testlighting/PrintQuestionActivity.kt
@@ -0,0 +1,21 @@
+package com.example.administrator.testlighting
+
+import android.app.Activity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_print_question.*
+
+class PrintQuestionActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_print_question)
+
+        printQuestion(Data.map)
+    }
+
+    private fun printQuestion(map: Map<String, String>) {
+        map.forEach { t, u ->
+            tv.append("$t\n")
+        }
+    }
+}
