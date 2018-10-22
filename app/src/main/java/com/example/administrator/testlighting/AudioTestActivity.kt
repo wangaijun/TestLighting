3c99a186098bf81d84e08417965b556d8495c92c
hw_wangaijun@163.com <waj67011>
2018-10-23 05:14:52 +0800

    添加语音测试窗体

--- app/src/main/java/com/example/administrator/testlighting/AudioTestActivity.kt
+++ app/src/main/java/com/example/administrator/testlighting/AudioTestActivity.kt
@@ -3,9 +3,11 @@ package com.example.administrator.testlighting
 import android.app.Activity
 import android.media.MediaPlayer
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_audio_test.*
+import java.util.*
 
 class AudioTestActivity : Activity() {
-    val arr = arrayListOf(
+    private val arr = arrayListOf(
         R.raw.start,
         R.raw.a0,
         R.raw.a1,
@@ -22,21 +24,45 @@ class AudioTestActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_test)
 
+        btnAllAudio.setOnClickListener {
+            playAllMp3()
+        }
+
+        btnAudioTest.setOnClickListener {
+            playOneGroupMp3()
+        }
+    }
+
+    private fun playOneGroupMp3() {
         object : Thread(){
             override fun run() {
-                playMp3(R.raw.start)
+                val list = mutableListOf<Int>()
+                list.addAll(arr.subList(1,10))
+                list.shuffle(Random(System.currentTimeMillis()))
+                val r = list.subList(0,5)
+                r.add(0,arr[0])
+                r.add(arr[10])
+                playMp3(R.raw.start,r)
             }
         }.start()
     }
 
-    private fun playMp3(id: Int) {
+    private fun playAllMp3() {
+        object : Thread(){
+            override fun run() {
+                playMp3(R.raw.start, arr)
+            }
+        }.start()
+    }
+
+    private fun playMp3(id: Int, arr: List<Int>) {
         val mp = MediaPlayer.create(this@AudioTestActivity, id)
         mp.start()
         mp.setOnCompletionListener {
             Thread.sleep(5000)
             val index = arr.indexOf(id)
             if (index<arr.size-1){
-                playMp3(arr[index+1])
+                playMp3(arr[index+1],arr)
             }
         }
     }
