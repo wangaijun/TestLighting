77fa1ebbbd3b14720162ddf2b62792833eaa9bf1
hw_wangaijun@163.com <waj67011>
2018-10-23 04:56:25 +0800

    添加语音测试窗体

--- app/src/main/java/com/example/administrator/testlighting/AudioTestActivity.kt
+++ app/src/main/java/com/example/administrator/testlighting/AudioTestActivity.kt
@@ -5,16 +5,39 @@ import android.media.MediaPlayer
 import android.os.Bundle
 
 class AudioTestActivity : Activity() {
+    val arr = arrayListOf(
+        R.raw.start,
+        R.raw.a0,
+        R.raw.a1,
+        R.raw.a2,
+        R.raw.a3,
+        R.raw.a4,
+        R.raw.a5,
+        R.raw.a6,
+        R.raw.a7,
+        R.raw.a8,
+        R.raw.end
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_test)
 
         object : Thread(){
             override fun run() {
-                val mp = MediaPlayer.create(this@AudioTestActivity,R.raw.a0)
-                mp.start()
-                sleep(500)
+                playMp3(R.raw.start)
             }
         }.start()
+    }
+
+    private fun playMp3(id: Int) {
+        val mp = MediaPlayer.create(this@AudioTestActivity, id)
+        mp.start()
+        mp.setOnCompletionListener {
+            Thread.sleep(5000)
+            val index = arr.indexOf(id)
+            if (index<arr.size-1){
+                playMp3(arr[index+1])
+            }
+        }
     }
 }
